f57d90a83fc6f35184f6cabd45517c4a683f3991
ruspet <80217877+Denis-Grekov@users.noreply.github.com>
2023-04-09 21:51:08 +0300

    company

--- company/company/Program.cs
+++ company/company/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Graph;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,44 @@ namespace company
     {
         static void Main(string[] args)
         {
-
+            int post, toDo;
+            
             Company company = new Company();
             company.GenerateEmployees(10);
             company.PrintEmployees();
+            Console.WriteLine("Что сделать? 1 - показать определенных сотрудников; 2 - Удалить сотрудника с наименьшей зарплатой; 3 - поздароваться");
+
+            toDo = Convert.ToInt32(Console.ReadLine());
+
+            switch (toDo)
+            {
+                case 1:
+                    Console.WriteLine("Каких сотрудников вывести? 1 - Director; 2 - SeniorEmployee; 3 - JuniorEmployee");
+                    post = Convert.ToInt32(Console.ReadLine());
 
 
+                    if (post == 1 || post == 2 || post == 3)
+                    {
+                        company.PrintEmployeesWithPost(post);
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("Выберите из перечисленных!");
+                    }
+                    break;
+                case 2:
+                    company.RemoveEmployee();
+                    company.PrintEmployees();
+                    break;
+                case 3:
+                    company.Hello();
+                    break;
+                default:
+                    Console.WriteLine("Выберите из перечисленных!");
+                    break;
+            }
+            
             Console.ReadKey();
         }
 
@@ -33,24 +66,26 @@ namespace company
 
     class Employee
     {
-        public double salary { get; set; }
-        public string name { get; set; }
-        public int age { get; set; }
-        public string companyName { get; set; }
-        public PostEnum post { get; set; } // 1 - Директор, 2 - Старший сотрудник, 3 - Младший сотрудник
+        public double Salary { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string CompanyName { get; set; }
+        public PostEnum Post { get; set; } // 1 - Директор, 2 - Старший сотрудник, 3 - Младший сотрудник
+
+
 
         public void Hello()
         {
-            switch (post)
+            switch (Post)
             {
                 case PostEnum.Director:
-                    Console.WriteLine($"Здравствуйте, меня зовут {name}, надеюсь на наше дальнейшее сотрудничество!");
+                    Console.WriteLine($"Здравствуйте, меня зовут {Name}, надеюсь на наше дальнейшее сотрудничество!");
                     break;
                 case PostEnum.SeniorEmployee:
-                    Console.WriteLine($"Привет, я {name}, будем знакомы.");
+                    Console.WriteLine($"Привет, я {Name}, будем знакомы.");
                     break;
                 case PostEnum.JuniorEmployee:
-                    Console.WriteLine($"Ку, я {name}, теперь мы в одной лодке :)");
+                    Console.WriteLine($"Ку, я {Name}, теперь мы в одной лодке :)");
                     break;
             }
         }
@@ -87,11 +122,11 @@ namespace company
 
                 var employee = new Employee
                 {
-                    salary = Math.Round(rnd.NextDouble() * 2000 + 1000, 2),
-                    name = newName,
-                    age = rnd.Next(18, 100),
-                    companyName = companyNames[rnd.Next(companyNames.Count)],
-                    post = (PostEnum)rnd.Next(1, Enum.GetValues(typeof(PostEnum)).Length + 1)
+                    Salary = Math.Round(rnd.NextDouble() * 2000 + 1000, 2),
+                    Name = newName,
+                    Age = rnd.Next(18, 100),
+                    CompanyName = companyNames[rnd.Next(companyNames.Count)],
+                    Post = (PostEnum)rnd.Next(1, Enum.GetValues(typeof(PostEnum)).Length + 1)
             };
                 employees.Add(employee);
             }
@@ -101,13 +136,54 @@ namespace company
             Console.WriteLine("{0,-20} {1,-10} {2,-20} {3,-20} {4,-10}", "ФИО", "Возраст", "Должность", "Зарплата", "Компания");
             foreach (var employee in employees)
             {
-                Console.WriteLine("{0,-20} {1,-10} {2,-20} {3,-20} {4,-10}", employee.name, employee.age, employee.post, employee.salary, employee.companyName);
+                Console.WriteLine("{0,-20} {1,-10} {2,-20} {3,-20} {4,-10}", employee.Name, employee.Age, employee.Post, employee.Salary, employee.CompanyName);
             }
             Console.WriteLine();
         }
+
+       
+        public void PrintEmployeesWithPost(int post)
+        {
+
+            var filteredEmployees = employees.Where(e => (int)e.Post == post).OrderByDescending(e => e.Name);
+            Console.WriteLine("{0,-20} {1,-10} {2,-20} {3,-20} {4,-10}", "ФИО", "Возраст", "Должность", "Зарплата", "Компания");
+            foreach (var employee in filteredEmployees)
+            {
+                Console.WriteLine("{0,-20} {1,-10} {2,-20} {3,-20} {4,-10}", employee.Name, employee.Age, employee.Post, employee.Salary, employee.CompanyName);
+            }
+            Console.WriteLine();
+        }
+
+        public void RemoveEmployee()
+        {
+            var minSalary = employees.Min(e => e.Salary);
+            var employeeToRemove = employees.First(e => e.Salary == minSalary);
+            employees.Remove(employeeToRemove);
+        }
+
+        public void Hello()
+        {
+            List<Employee> directors = employees.Where(e => e.Post == PostEnum.Director).ToList();
+            List<Employee> seniorEmployees = employees.Where(e => e.Post == PostEnum.SeniorEmployee).ToList();
+            List<Employee> juniorEmployees = employees.Where(e => e.Post == PostEnum.JuniorEmployee).ToList();
+
+            
+            Random random = new Random();
+            Employee randomDirector = directors[random.Next(directors.Count)];
+            Console.WriteLine("Директор: ");
+            randomDirector.Hello();
+
+            Employee randomSeniorEmployee = seniorEmployees[random.Next(seniorEmployees.Count)];
+            Console.WriteLine("Старший сотрудник: ");
+            randomSeniorEmployee.Hello();
+
+            Employee randomJuniorEmployee = juniorEmployees[random.Next(juniorEmployees.Count)];
+            Console.WriteLine("Младший сотрудник: ");
+            randomJuniorEmployee.Hello();
+        }
     }
 
-
+    
 
     
 }
