97f437d0ab11f1863759cf5206982047c5958fda
ruspet <80217877+Denis-Grekov@users.noreply.github.com>
2023-04-18 00:35:50 +0300

    books

---
+++ books/books/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static SemaphoreSlim semaphore; 
+    static int numReaders; 
+    static Random random = new Random(); 
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите количество читателей (N): ");
+        if (int.TryParse(Console.ReadLine(), out numReaders))
+        {
+            semaphore = new SemaphoreSlim(2); 
+
+            for (int i = 0; i < numReaders; i++)
+            {
+                Reader reader = new Reader(i + 1); 
+                Thread readerThread = new Thread(reader.ReadBooks); // Создание потоков для работы читателей
+                readerThread.Start(); 
+            }
+
+            Console.ReadLine(); 
+        }
+        else
+        {
+            Console.WriteLine("Некорректное значение. Попробуйте еще раз.");
+        }
+    }
+
+    class Reader
+    {
+        private int readerId; 
+        private int numVisits; 
+
+        public Reader(int id)
+        {
+            readerId = id;
+            numVisits = 0;
+        }
+
+        public void ReadBooks()
+        {
+            Console.WriteLine($"Читатель {readerId} пришел в библиотеку.");
+
+            while (numVisits < 2) 
+            {
+                semaphore.Wait(); 
+
+                Console.WriteLine($"Читатель {readerId} зашел в библиотеку и начал читать.");
+
+                
+                Thread.Sleep(random.Next(1000, 5000));
+
+                Console.WriteLine($"Читатель {readerId} закончил читать и покинул библиотеку.");
+
+                semaphore.Release(); // Освобождение разрешения на вход в библиотеку
+
+                numVisits++; 
+
+                
+                Thread.Sleep(random.Next(1000, 5000));
+            }
+        }
+    }
+}
