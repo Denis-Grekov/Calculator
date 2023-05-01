d4ee684cdc40fa40983d1cd4e5134ee730d625a6
ruspet <80217877+Denis-Grekov@users.noreply.github.com>
2023-05-01 23:24:37 +0300

    model

---
+++ cakcuulatorNew/cakcuulatorNew/Interfaces/ICalculatorModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cakcuulatorNew.Interfaces
+{
+    interface ICalculatorModel
+    {
+        double Num1 { get; set; }
+        double Num2 { get; set; }
+        double Result { get; set; }
+        bool CanDivide { get; }
+    }
+
+    class CalculatorModel : ICalculatorModel
+    {
+        private double num1;
+        private double num2;
+        private double result;
+
+        public double Num1
+        {
+            get => num1;
+            set
+            {
+                if (num1 != value)
+                {
+                    num1 = value;
+                }
+            }
+        }
+
+        public double Num2
+        {
+            get => num2;
+            set
+            {
+                if (num2 != value)
+                {
+                    num2 = value;
+                }
+            }
+        }
+
+        public double Result
+        {
+            get => result;
+            set
+            {
+                if (result != value)
+                {
+                    result = value;
+                }
+            }
+        }
+
+        public bool CanDivide
+        {
+            get => num2 != 0;
+        }
+
+        public void Add()
+        {
+            Result = Num1 + Num2;
+        }
+
+        public void Subtract()
+        {
+            Result = Num1 - Num2;
+        }
+
+        public void Multiply()
+        {
+            Result = Num1 * Num2;
+        }
+
+        public void Divide()
+        {
+            if (CanDivide)
+            {
+                Result = Num1 / Num2;
+            }
+            else
+            {
+                throw new DivideByZeroException("На ноль делить нельзя");
+            }
+        }
+    }
+}
---
+++ cakcuulatorNew/cakcuulatorNew/Interfaces/ICar.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cakcuulatorNew.Interfaces
+{
+    interface ICar
+    {
+        string Brand { get; }
+        double HorsePower { get;  }
+        int YearOfRealese { get; }
+
+        int MaxSpeed();
+
+        
+
+        
+    }
+
+    class BMW : ICar
+    {
+        public string Brand => "BMW";
+
+        public double HorsePower => 200;
+
+        public int YearOfRealese => 2003;
+
+        public int MaxSpeed()
+        {
+            int MaxSpeed = 300;
+            return MaxSpeed;
+        }
+    }
+    class Marcedes : ICar
+    {
+        public string Brand => "Marcedes";
+
+        public double HorsePower => 210;
+
+        public int YearOfRealese => 2010;
+
+        public int MaxSpeed()
+        {
+            int MaxSpeed = 320;
+            return MaxSpeed;
+        }
+    }
+
+}
