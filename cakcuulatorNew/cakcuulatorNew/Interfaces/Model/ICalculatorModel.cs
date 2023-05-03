275cf8771cf1cc6f2ce17c720013bb12fe51b42f
ruspet <80217877+Denis-Grekov@users.noreply.github.com>
2023-05-03 23:41:24 +0300

    viewModel

---
+++ cakcuulatorNew/cakcuulatorNew/Interfaces/Model/ICalculatorModel.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cakcuulatorNew.Interfaces
+{
+    interface ICalculatorModel
+    {
+        double Num1 { get; set; }
+        double Num2 { get; set; }
+        double Result { get; set; }
+        void Add();
+        void Subtract();
+        void Multiply();
+        void Divide();
+    }
+
+    class CalculatorModel : ICalculatorModel
+    {
+        private double num1;
+        private double num2;
+        private double result;
+
+        public double Num1
+        {
+            get => num1;
+            set
+            {
+                if (num1 != value)
+                {
+                    num1 = value;
+                }
+            }
+        }
+
+        public double Num2
+        {
+            get => num2;
+            set
+            {
+                if (num2 != value)
+                {
+                    num2 = value;
+                }
+            }
+        }
+
+        public double Result
+        {
+            get => result;
+            set
+            {
+                if (result != value)
+                {
+                    result = value;
+                }
+            }
+        }
+
+
+        public void Add()
+        {
+            Result = Num1 + Num2;
+        }
+
+        public void Subtract()
+        {
+            Result = Num1 - Num2;
+        }
+
+        public void Multiply()
+        {
+            Result = Num1 * Num2;
+        }
+
+        public void Divide()
+        {
+            if (Num2 != 0)
+            {
+                Result = Num1 / Num2;
+            }
+            else
+            {
+                throw new DivideByZeroException("На ноль делить нельзя");
+            }
+        }
+    }
+}
---
+++ cakcuulatorNew/cakcuulatorNew/Interfaces/ViewModel/ICalculatorViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+using cakcuulatorNew.Interfaces;
+
+namespace cakcuulatorNew.ViewModels
+{
+    class CalculatorViewModel : INotifyPropertyChanged
+    {
+        private readonly ICalculatorModel _calculator;
+
+        public CalculatorViewModel(ICalculatorModel calculator)
+        {
+            _calculator = calculator;
+        }
+
+        public double Num1
+        {
+            get => _calculator.Num1;
+            set
+            {
+                _calculator.Num1 = value;
+                OnPropertyChanged(nameof(Num1));
+            }
+        }
+
+        public double Num2
+        {
+            get => _calculator.Num2;
+            set
+            {
+                _calculator.Num2 = value;
+                OnPropertyChanged(nameof(Num2));
+            }
+        }
+
+        public double Result => _calculator.Result;
+
+        public void Add()
+        {
+            _calculator.Add();
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public void Subtract()
+        {
+            _calculator.Subtract();
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public void Multiply()
+        {
+            _calculator.Multiply();
+            OnPropertyChanged(nameof(Result));
+        }
+
+        public void Divide()
+        {
+            
+            _calculator.Divide();
+            OnPropertyChanged(nameof(Result));
+            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
