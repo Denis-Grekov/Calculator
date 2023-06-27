abcef8329686aa4685ba70a81a6485da3ba5e76d
ruspet <80217877+Denis-Grekov@users.noreply.github.com>
2023-06-27 18:44:00 +0300

    reflection full new

---
+++ Reflection2/Reflection2/Foo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection2
+{
+    class Foo
+    {
+        public string TestString;
+
+       public int TestNumber { get; set; }
+
+        private char TestChar { get; set; }
+
+    }
+}
--- Reflection2/Reflection2/Program.cs
+++ Reflection2/Reflection2/Program.cs
@@ -1,74 +1,58 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using Reflection2;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Введите путь до файла: ");
-        string filePath = Console.ReadLine();
-
-        if (!File.Exists(filePath))
-        {
-            Console.WriteLine("Файл не найден.");
-            return;
-        }
+        
 
         try
         {
-            string[] lines = File.ReadAllLines(filePath);
-            string className = null;
-            bool isInClass = false;
+             var assembly = Assembly.GetExecutingAssembly();
 
-            foreach (string line in lines)
+
+
+            foreach (var type in assembly.GetTypes())
             {
-                if (line.Contains("class"))
+                var className = type.Name; 
+
+                foreach (var field in type.GetFields())
                 {
-                    int startIndex = line.IndexOf("class") + 5;
-                    int endIndex = line.IndexOfAny(new char[] { ' ', ':' }, startIndex);
+                    var level = "default";
+                    if (field.IsPublic)
+                        level = "public";
+                    else if (field.IsPrivate)
+                        level = "private";
 
-                    if (endIndex == -1)
-                        endIndex = line.Length;
-
-                    className = line.Substring(startIndex, endIndex - startIndex);
-                    isInClass = true;
+                    Console.WriteLine($"{className} {level} field {field.FieldType} {field.Name}");
                 }
-                else if (isInClass && (line.Contains("public") || line.Contains("private") || line.Contains("protected") || line.Contains("internal")))
-                {
-                    string[] parts = line.Split(new char[] { ' ', ';' }, StringSplitOptions.RemoveEmptyEntries);
 
-                    if (parts.Length >= 3)
+                foreach (var property in type.GetProperties())
+                {
+                    var access = property.GetAccessors().FirstOrDefault();
+
+                    var level = "default";
+                    if (access != null)
                     {
-                        string accessModifier = parts[0];
-                        string fieldType = parts[1];
-                        string fieldName = parts[2];
-
-                        Console.WriteLine($"{accessModifier} field {fieldName} {fieldType}");
+                        if (access.IsPublic)
+                            level = "public";
+                        else if (access.IsPrivate)
+                            level = "private";
                     }
-                }
-                else if (isInClass && line.Contains("{ get;") && line.Contains("set;"))
-                {
-                    string[] parts = line.Split(new char[] { ' ', '{', '}', ';', '\t' }, StringSplitOptions.RemoveEmptyEntries);
 
-                    if (parts.Length >= 3)
-                    {
-                        string accessModifier = parts[0];
-                        string propertyType = parts[1];
-                        string propertyName = parts[2];
+                    var getEnable = property.CanRead ? "GetEnable" : "GetDisable";
+                    var setEnable = property.CanWrite ? "SetEnable" : "SetDisable";
 
-                        Console.WriteLine($"{accessModifier} property {propertyName} {propertyType} GetEnable SetEnable");
-                    }
+                    Console.WriteLine($"{className} {level} property {property.PropertyType} {property.Name} {getEnable} {setEnable}");
                 }
             }
 
-            if (!isInClass)
-            {
-                Console.WriteLine("В файле не найден класс.");
-            }
-            else
-            {
-                Console.WriteLine($"В файле \"{filePath}\" найден класс {className}.");
-            }
+
         }
         catch (Exception ex)
         {
